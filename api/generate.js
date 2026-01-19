b7541a3400e8ee4f965a3e59e41e577a433bcce0
Candra2222 <84482710+Candra2222@users.noreply.github.com>
2026-01-19 16:43:46 +0700

    Create generate.js

---
+++ api/generate.js
@@ -0,0 +1,10 @@
+export default function handler(req, res) {
+  const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let kode = '';
+  for (let i = 0; i < 10; i++) {
+    kode += chars[Math.floor(Math.random() * chars.length)];
+  }
+
+  const link = `https://profound-nasturtium-68c219.netlify.app/${kode}`;
+  res.status(200).json({ link });
+}
