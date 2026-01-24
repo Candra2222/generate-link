443974e7bfcd29594fe13d2d42a8f9e84948f5db
Candra <84482710+Candra2222@users.noreply.github.com>
2026-01-24 12:39:31 +0700

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -1,10 +1,22 @@
 export default function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+
+  const { base } = req.body;
+
+  if (!base) {
+    return res.status(400).json({
+      error: 'base link wajib diisi'
+    });
+  }
+
   const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
   let kode = '';
   for (let i = 0; i < 10; i++) {
     kode += chars[Math.floor(Math.random() * chars.length)];
   }
 
-  const link = `https://zingy-chaja-2b9c84.netlify.app/?${kode}`;
+  const link = `${base}?${kode}`;
   res.status(200).json({ link });
 }
